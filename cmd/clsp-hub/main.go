2b1d405f0851197ef8c8284a5f0b966568eee48b
T-1-M <18md10@michener.ca>
2025-05-25 01:27:35 -0400

    Add init command to clsp-hub for environment setup

--- cmd/clsp-hub/main.go
+++ cmd/clsp-hub/main.go
@@ -7,15 +7,34 @@ import (
 	"net/http"
 	"os"
 	"os/signal"
+	"path/filepath"
 	"syscall"
 
 	"github.com/mattd/clsp/internal/hub"
 )
 
+func doInit(dbPath string) {
+	dir := filepath.Dir(dbPath)
+	if err := os.MkdirAll(dir, 0700); err != nil {
+		log.Fatalf("Failed to create directory %s: %v", dir, err)
+	}
+	server, err := hub.NewServer(dbPath)
+	if err != nil {
+		log.Fatalf("Failed to initialize database: %v", err)
+	}
+	server.Shutdown() // Close DB connection
+	fmt.Printf("Initialization successful! Directory '%s' and database '%s' are ready.\n", dir, dbPath)
+}
+
 func main() {
 	port := flag.Int("port", 8080, "Port to listen on")
 	dbPath := flag.String("db", ".clsp/hub.db", "Path to database file")
 	flag.Parse()
+
+	if len(flag.Args()) > 0 && flag.Args()[0] == "init" {
+		doInit(*dbPath)
+		return
+	}
 
 	// Create server with database path
 	server, err := hub.NewServer(*dbPath)
